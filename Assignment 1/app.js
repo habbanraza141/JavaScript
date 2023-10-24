53405ce8e9f6e0d303554b03eae9e7fead48cc5b
muhammad habban raza <habbanraza@gmail.com>
2023-10-24 12:56:58 +0500

    two assignments

---
+++ Assignment 1/app.js
@@ -0,0 +1,10 @@
+// alert('Welcome to the website!');
+
+// alert('Error! Please Enter a valid password');
+
+// alert('Welcome to JS land! \n Happy coding');
+
+// alert('Welcome to JS land!');
+// alert('Happy coding')
+
+alert('Hello! I can run javascript through my browser console!')
---
+++ Assignment 2/app.js
@@ -0,0 +1,32 @@
+// const userName = "Habban";
+// console.log(userName);
+
+// const myName = "Muhammad Habban Raza";
+// console.log(myName);
+
+// const message = "Hello world";
+// alert(message);
+
+// let name1 = 'Habban'
+// let age = "25 years old"
+// let school = "Expertizo University"
+// let batch = "EX21 Fast Track beginners"
+// alert(name1)
+// alert(age)
+// alert(school)
+// alert(batch)
+
+// let numberChildren = 5;
+// let partnersName = "Umme Ahmed"
+// let geoLocation = "Karachi"
+// let jobTitle = "React Native Developer"
+// alert("You will be a software developer in " + geoLocation + " as a " + jobTitle + " and married to " + partnersName + " with " + numberChildren + " children")
+
+// var email = "habbanraza@gmail.com"
+// alert("My email address is " + email)
+
+// var book = "A smarter way to learn JavaScript"
+// alert("I am learning " + book + " these days")
+
+
+// document.write("Hey! I can write HTML content through JavaScript");
