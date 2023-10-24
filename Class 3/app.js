e2ba6dc64b445d0384caf80b5feebaa8856953fc
muhammad habban raza <habbanraza@gmail.com>
2023-10-24 09:50:59 +0500

    new update

---
+++ Class 3/app.js
@@ -0,0 +1,23 @@
+// const percentage = 56;
+
+// if (percentage > 80 && percentage <= 70) {
+//     console.log('grade A+')
+// } else if (percentage > 70 && percentage <= 60) {
+//     console.log('grade A')
+// } else if (percentage > 60 && percentage <= 50) {
+//     console.log('grade B')
+// } else (percentage > 80 && percentage <= 70) {
+//     console.log('grade F')
+// }
+
+
+
+const wicket = Number(prompt("Enter the number of wickets"));
+
+if (wicket > 0 && wicket <= 4) {
+    console.log('pakistan haar gaya')
+} else if (wicket > 4 && wicket <= 6) {
+    console.log('match draw')
+} else {
+    console.log('pakistan jeet gaya')
+} 
