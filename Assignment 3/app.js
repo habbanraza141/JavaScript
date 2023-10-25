9098dcb8d8b89a535e372e3f073f779323e5a9a3
muhammad habban raza <habbanraza@gmail.com>
2023-10-25 08:20:03 +0500

    third assignment

---
+++ Assignment 3/app.js
@@ -0,0 +1,16 @@
+// let age = 25;
+// // alert("I am " + age + " years old")
+
+// let N = 10;
+// alert("You have visited the website " + N + " times")
+
+// let birthYear = 1998;
+// document.write("My birth year is" + birthYear + "  \n Data type of my birth year is " + typeof (birthYear))
+
+// let visitorName = "Habban Raza"
+// let productTitle = " shirts"
+// let quantity = " 6"
+// document.write(visitorName + " has ordered" + quantity + productTitle)
+
+let [firstName, lastName, age] = ["Habban", " Raza", 25];
+console.log("my name is " + firstName + lastName + " and my age is " + age);
