c6f187f55e408d1c60c94a05d5b91358890cfc5e
muhammad habban raza <habbanraza@gmail.com>
2023-10-28 12:20:02 +0500

    work done

---
+++ Class 4/app.js
@@ -0,0 +1,65 @@
+// Question number: 1
+// function greetUser() {
+//     console.log("hello");
+//     console.log("hello");
+//     console.log("hello");
+//     console.log("hello");
+//     console.log("hello");
+// }
+// greetUser();
+
+
+// Question number: 2
+// function greetUser(names, age, salary) {
+//     console.log("my name is " + names + " and my age is " + age + " and my salary is " + salary);;
+// }
+// greetUser("habban", 25, 50000);
+
+
+
+// Question number: 3
+// let names = prompt("Enter you name: ");
+// let age = prompt("Enter you age: ");
+// let salary = prompt("Enter you salary: ");
+// function greetUser(names, age, salary) {
+//     console.log("my name is " + names + " and my age is " + age + " and my salary is " + salary);;
+// }
+// greetUser(names, age, salary);
+
+
+// Question number: 4
+// function sumOfTwoNum(num1, num2) {
+//     const sum = num1 + num2
+//     return sum
+// }
+// console.log(sumOfTwoNum(9, 8));
+
+
+// Exercise
+// write a function to take data from the user and print its details and total cost
+// function names() {
+//     let name1 = prompt("Enter you name: ");
+//     let product1 = prompt("Enter you product1 name: ");
+//     let product2 = prompt("Enter you product2 name: ");
+//     let cost1 = +prompt("Enter cost1 : ");
+//     let cost2 = +prompt("Enter cost2 : ");
+//     let totalAmount = cost1 + cost2
+//     console.log("my name is " + name1 + " and product 1 name is " + product1 + " and product 1 cost is " + cost1 + " and product 2 name is " + product2 + " and product 2 cost is " + cost2 + " and the total amount is " + totalAmount)
+//     return names
+// }
+// names();
+
+
+// Exercise
+// write a function to take data from the user and print its details and total cost
+// let name1 = prompt("Enter you name: ");
+// let product1 = prompt("Enter you product1 name: ");
+// let product2 = prompt("Enter you product2 name: ");
+// let cost1 = +prompt("Enter cost1 : ");
+// let cost2 = +prompt("Enter cost2 : ");
+// const totalAmount = cost1 + cost2;
+// function names() {
+//     return totalAmount
+// }
+// console.log("my name is " + name1 + " and product 1 name is " + product1 + " and product 1 cost is " + cost1 + " and product 2 name is " + product2 + " and product 2 cost is " + cost2 + " and the total amount is " + names())
+
---
+++ Some other examples/app.js
@@ -0,0 +1,24 @@
+// switch case example
+// const name1 = "Maaz"
+// switch (name1) {
+//     case "Maaz":
+//         console.log("Your answer is wrong");
+//         break;
+//     case "Abdullah":
+//         console.log("Your answer is wrong");
+//         break;
+//     case "Habban":
+//         console.log("Your answer is correct");
+
+// }
+
+
+// ternary operator
+
+// const age = +prompt("Enter your age")
+// if (age > 18) {
+//     alert("You can drive");
+// } else {
+//     alert("You can not drive");
+// }
+// console.log("You can", (age < 18 ? "not drive" : "drive"));
