c3562273cbc99d0b4bdbe0b20d5a68954b5fa111
muhammad habban raza <habbanraza@gmail.com>
2023-10-30 18:39:24 +0500

    work finished

--- Assignments/Assignment 6/app.js
+++ Assignments/Assignment 6/app.js
@@ -2,28 +2,106 @@
 // alert("Hi " + a)
 
 
-let n = +prompt("Enter the number")
-if (n == 0) {
-    console.log("5 X 1 = 5");
-    console.log("5 X 2 = 10");
-    console.log("5 X 3 = 15");
-    console.log("5 X 4 = 20");
-    console.log("5 X 5 = 25");
-    console.log("5 X 6 = 30");
-    console.log("5 X 7 = 35");
-    console.log("5 X 8 = 40");
-    console.log("5 X 9 = 45");
-    console.log("5 X 10 = 50");
+// let n = +prompt("Enter the number")
+// if (n == 0) {
+//     console.log("5 X 1 = 5");
+//     console.log("5 X 2 = 10");
+//     console.log("5 X 3 = 15");
+//     console.log("5 X 4 = 20");
+//     console.log("5 X 5 = 25");
+//     console.log("5 X 6 = 30");
+//     console.log("5 X 7 = 35");
+//     console.log("5 X 8 = 40");
+//     console.log("5 X 9 = 45");
+//     console.log("5 X 10 = 50");
 
+// } else {
+//     console.log(n + " X 1 = " + n * 1);
+//     console.log(n + " X 2 = " + n * 2);
+//     console.log(n + " X 3 = " + n * 3);
+//     console.log(n + " X 4 = " + n * 4);
+//     console.log(n + " X 5 = " + n * 5);
+//     console.log(n + " X 6 = " + n * 6);
+//     console.log(n + " X 7 = " + n * 7);
+//     console.log(n + " X 8 = " + n * 8);
+//     console.log(n + " X 9 = " + n * 9);
+//     console.log(n + " X 10 = " + n * 10);
+// }
+
+
+
+// let city = prompt("Enter the name of the city")
+// if (city === "Karachi") {
+//     console.log("Welcome to the city of lights");
+// } else {
+// }
+
+
+
+// let gender = prompt("Enter your gender")
+// if (gender === "male") {
+//     console.log("Good morning Sir");
+// } else if (gender === "female") {
+//     console.log("Good morning Maam");
+// } else {
+
+// }
+
+// let color = prompt("Enter the right color")
+// if (color === "red") {
+//     console.log("vehicles must stop")
+// } else if (color === "yellow") {
+//     console.log("vehicles should get ready to move")
+// } else if (color === "green") {
+//     console.log("vehicles can move now")
+// }
+// else {
+// }
+
+
+let score = +prompt("Enter the score")
+if (score > 50) {
+    console.log("You are passed")
 } else {
-    console.log(n + " X 1 = " + n * 1);
-    console.log(n + " X 2 = " + n * 2);
-    console.log(n + " X 3 = " + n * 3);
-    console.log(n + " X 4 = " + n * 4);
-    console.log(n + " X 5 = " + n * 5);
-    console.log(n + " X 6 = " + n * 6);
-    console.log(n + " X 7 = " + n * 7);
-    console.log(n + " X 8 = " + n * 8);
-    console.log(n + " X 9 = " + n * 9);
-    console.log(n + " X 10 = " + n * 10);
+    console.log("try again")
+
 }
+
+// let a = +prompt("Enter the number")
+// let b = +prompt("Enter the number")
+// if (a > b) {
+//     console.log("The greater number of " + a + " and " + b + " is " + a)
+// } else if (a < b) {
+//     console.log("The greater number of " + a + " and " + b + " is " + b)
+// } else {
+//     console.log("Both are equal numbers")
+
+// }
+
+
+
+// let language = prompt("Enter the language")
+// if (language === "es") {
+//     console.log("Hola Mundo")
+// } else if (language === "de") {
+//     console.log("Hallo Wereld")
+// } else if (language === "en") {
+//     console.log("Hello, world")
+
+// } else {
+
+// }
+
+// let number = +prompt("Enter the number")
+// if (number < 0) {
+//     console.log("negative")
+// } else {
+//     console.log("positive")
+
+// }
+
+
+
+// let number = +prompt("Enter the number")
+// let noun = prompt("Enter the noun")
+// alert(number + " " + noun)
---
+++ Class 5/app.js
@@ -0,0 +1,10 @@
+const h1 = document.querySelector('.head');
+
+
+function greetUser() {
+    if (h1.innerHTML === 'changed through javascript') {
+        h1.innerHTML = 'hello world'
+    } else {
+        h1.innerHTML = 'changed through javascript'
+    }
+}
